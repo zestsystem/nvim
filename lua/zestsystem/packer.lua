76625d146ffbb4ac4ffd536e0bb7fa8469aa95ca
Mike Yim <mikeyim@unitetheculture.org>
2023-03-20 21:05:46 -0700

    feat: add purescript plugins

--- lua/zestsystem/packer.lua
+++ lua/zestsystem/packer.lua
@@ -1,57 +1,60 @@
 vim.cmd.packadd("packer.nvim")
 
 return require("packer").startup(function(use)
-    use("wbthomason/packer.nvim")
+	use("wbthomason/packer.nvim")
 
-    use({ "nvim-telescope/telescope.nvim", tag = "0.1.0", requires = { { "nvim-lua/plenary.nvim" } } })
+	use({ "nvim-telescope/telescope.nvim", tag = "0.1.0", requires = { { "nvim-lua/plenary.nvim" } } })
 
-    use("nvim-treesitter/nvim-treesitter", {
-        run = ":TSUpdate",
-    })
-    use("nvim-treesitter/playground")
-    use("ThePrimeagen/harpoon")
-    use("mbbill/undotree")
-    use("tpope/vim-fugitive")
+	use("nvim-treesitter/nvim-treesitter", {
+		run = ":TSUpdate",
+	})
+	use("nvim-treesitter/playground")
+	use("ThePrimeagen/harpoon")
+	use("mbbill/undotree")
+	use("tpope/vim-fugitive")
 
-    -- Colorscheme section
-    use("gruvbox-community/gruvbox")
-    -- use("folke/tokyonight.nvim")
-    use({
-        "rose-pine/neovim",
-        as = "rose-pine",
-        config = function()
-            vim.cmd("colorscheme rose-pine")
-        end,
-    })
+	-- Colorscheme section
+	use("gruvbox-community/gruvbox")
+	-- use("folke/tokyonight.nvim")
+	use({
+		"rose-pine/neovim",
+		as = "rose-pine",
+		config = function()
+			vim.cmd("colorscheme rose-pine")
+		end,
+	})
 
-    --Debugger
-    use("mfussenegger/nvim-dap")
-    use("rcarriga/nvim-dap-ui")
-    use("theHamsta/nvim-dap-virtual-text")
-    use("lewis6991/gitsigns.nvim")
+	--Debugger
+	use("mfussenegger/nvim-dap")
+	use("rcarriga/nvim-dap-ui")
+	use("theHamsta/nvim-dap-virtual-text")
+	use("lewis6991/gitsigns.nvim")
 
-    use({
-        "VonHeikemen/lsp-zero.nvim",
-        requires = {
-            -- LSP Support
-            { "neovim/nvim-lspconfig" },
-            { "williamboman/mason.nvim" },
-            { "williamboman/mason-lspconfig.nvim" },
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		requires = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" },
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
 
-            -- Autocompletion
-            { "hrsh7th/nvim-cmp" },
-            { "hrsh7th/cmp-buffer" },
-            { "hrsh7th/cmp-path" },
-            { "saadparwaiz1/cmp_luasnip" },
-            { "hrsh7th/cmp-nvim-lsp" },
-            { "hrsh7th/cmp-nvim-lua" },
+			{ "purescript-contrib/purescript-vim" },
+			{ "FrigoEU/psc-ide-vim" },
 
-            -- Snippets
-            { "L3MON4D3/LuaSnip" },
-            { "rafamadriz/friendly-snippets" },
-        },
-    })
-    use("sbdchd/neoformat")
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-path" },
+			{ "saadparwaiz1/cmp_luasnip" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-nvim-lua" },
 
-    use("github/copilot.vim")
+			-- Snippets
+			{ "L3MON4D3/LuaSnip" },
+			{ "rafamadriz/friendly-snippets" },
+		},
+	})
+	use("sbdchd/neoformat")
+
+	use("github/copilot.vim")
 end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -194,6 +194,16 @@ _G.packer_plugins = {
     path = "/Users/mikeyim/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
+  ["psc-ide-vim"] = {
+    loaded = true,
+    path = "/Users/mikeyim/.local/share/nvim/site/pack/packer/start/psc-ide-vim",
+    url = "https://github.com/FrigoEU/psc-ide-vim"
+  },
+  ["purescript-vim"] = {
+    loaded = true,
+    path = "/Users/mikeyim/.local/share/nvim/site/pack/packer/start/purescript-vim",
+    url = "https://github.com/purescript-contrib/purescript-vim"
+  },
   ["rose-pine"] = {
     config = { "\27LJ\2\n9\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\26colorscheme rose-pine\bcmd\bvim\0" },
     loaded = true,
