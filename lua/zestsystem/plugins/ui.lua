f6ea627225bb19bfc87639b0a7333c9c4b24147c
Mike Yim <mikeyim@unitetheculture.org>
2023-07-18 19:10:29 -0700

    fix: remove noice nav cuz <s-f> is not it

--- lua/zestsystem/plugins/ui.lua
+++ lua/zestsystem/plugins/ui.lua
@@ -274,26 +274,6 @@ return {
                 desc =
                 "Dismiss All"
             },
-            {
-                "<S-f>",
-                function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end,
-                silent = true,
-                expr = true,
-                desc =
-                "Scroll forward",
-                mode = {
-                    "i", "n", "s" }
-            },
-            {
-                "<c-b>",
-                function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end,
-                silent = true,
-                expr = true,
-                desc =
-                "Scroll backward",
-                mode = {
-                    "i", "n", "s" }
-            },
         },
 	},
 
