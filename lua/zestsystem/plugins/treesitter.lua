f2cc07c15e46a41be8c9b235dada34e82ffbd276
Mike Yim <mikeyim@unitetheculture.org>
2023-07-22 16:43:51 -0700

    fix: fix treesitter right copy lazy all the way

--- lua/zestsystem/plugins/treesitter.lua
+++ lua/zestsystem/plugins/treesitter.lua
@@ -1,99 +1,93 @@
+local load_textobjects = false
 return {
-  { "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
+	-- Treesitter is a new parser generator tool that we can
+	-- use in Neovim to power faster and more accurate
+	-- syntax highlighting.
+	{
+		"nvim-treesitter/nvim-treesitter",
+		version = false, -- last release is way too old and doesn't work on Windows
+		build = ":TSUpdate",
+		event = { "BufReadPost", "BufNewFile" },
+		dependencies = {
+			{
+				"nvim-treesitter/nvim-treesitter-textobjects",
+				init = function()
+					-- disable rtp plugin, as we only need its queries for mini.ai
+					-- In case other textobject modules are enabled, we will load them
+					-- once nvim-treesitter is loaded
+					require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
+					load_textobjects = true
+				end,
+			},
+		},
+		cmd = { "TSUpdateSync" },
+		keys = {
+			{ "<c-space>", desc = "Increment selection" },
+			{ "<bs>", desc = "Decrement selection", mode = "x" },
+		},
+		---@type TSConfig
+		opts = {
+			highlight = { enable = true },
+			indent = { enable = true },
+			ensure_installed = {
+				"bash",
+				"c",
+				"html",
+				"javascript",
+				"json",
+				"lua",
+				"luadoc",
+				"luap",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"query",
+				"regex",
+				"tsx",
+				"typescript",
+				"vim",
+				"vimdoc",
+				"yaml",
+			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
+			},
+		},
+		---@param opts TSConfig
+		config = function(_, opts)
+			if type(opts.ensure_installed) == "table" then
+				---@type table<string, boolean>
+				local added = {}
+				opts.ensure_installed = vim.tbl_filter(function(lang)
+					if added[lang] then
+						return false
+					end
+					added[lang] = true
+					return true
+				end, opts.ensure_installed)
+			end
+			require("nvim-treesitter.configs").setup(opts)
 
-  {
-    "nvim-treesitter/nvim-treesitter-context",
-    event = "BufReadPre",
-    enabled = true,
-    opts = { mode = "cursor" },
-  },
-
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "astro",
-        "bash",
-        "c",
-        "cmake",
-        -- "comment", -- comments are slowing down TS bigtime, so disable for now
-        "cpp",
-        "css",
-        "diff",
-        "fish",
-        "gitignore",
-        "go",
-        "graphql",
-        "html",
-        "http",
-        "java",
-        "javascript",
-        "jsdoc",
-        "jsonc",
-        "lua",
-        "luap",
-        "markdown",
-        "markdown_inline",
-        "meson",
-        "ninja",
-        "nix",
-        "norg",
-        "org",
-        "php",
-        "python",
-        "query",
-        "regex",
-        "rust",
-        "scss",
-        "sql",
-        "svelte",
-        "teal",
-        "toml",
-        "tsx",
-        "typescript",
-        "vhs",
-        "vim",
-        "vimdoc",
-        "vue",
-        "wgsl",
-        "yaml",
-        -- "wgsl",
-        "json",
-        -- "markdown",
-      },
-      matchup = {
-        enable = true,
-      },
-      -- highlight = { enable = true },
-      query_linter = {
-        enable = true,
-        use_virtual_text = true,
-        lint_events = { "BufWrite", "CursorHold" },
-      },
-      playground = {
-        enable = true,
-        disable = {},
-        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-        persist_queries = true, -- Whether the query persists across vim sessions
-        keybindings = {
-          toggle_query_editor = "o",
-          toggle_hl_groups = "i",
-          toggle_injected_languages = "t",
-          toggle_anonymous_nodes = "a",
-          toggle_language_display = "I",
-          focus_language = "f",
-          unfocus_language = "F",
-          update = "R",
-          goto_node = "<cr>",
-          show_help = "?",
-        },
-      },
-    },
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      { "windwp/nvim-ts-autotag", opts = {} },
-    },
-  },
+			if load_textobjects then
+				-- PERF: no need to load the plugin, if we only need its queries for mini.ai
+				if opts.textobjects then
+					for _, mod in ipairs({ "move", "select", "swap", "lsp_interop" }) do
+						if opts.textobjects[mod] and opts.textobjects[mod].enable then
+							local Loader = require("lazy.core.loader")
+							Loader.disabled_rtp_plugins["nvim-treesitter-textobjects"] = nil
+							local plugin = require("lazy.core.config").plugins["nvim-treesitter-textobjects"]
+							require("lazy.core.loader").source_runtime(plugin.dir, "plugin")
+							break
+						end
+					end
+				end
+			end
+		end,
+	},
 }
